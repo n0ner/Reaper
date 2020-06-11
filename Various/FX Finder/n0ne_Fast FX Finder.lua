e508a4dda80bca386126c91faf6846de6cc1b15e
n0ne <57045713+n0ne@users.noreply.github.com>
2020-06-11 19:12:54 +0200

    Update n0ne_Fast FX Finder.lua

--- Various/FX Finder/n0ne_Fast FX Finder.lua
+++ Various/FX Finder/n0ne_Fast FX Finder.lua
@@ -7,8 +7,10 @@
 	A little window that allows for quick searching of FX (can be VST, templates or fxrack).
 
 	The script stores how often you select a certain FX and orders the list by how many times something is used.
-@version 0.7.25
+@version 0.7.26
 @changelog
+	0.7.26
+	+ added more comments to default settings file to make it easier to costumize things
 	0.7.25
 	+ Fix window focus when opening in dock
 	0.7.24
